48c2d1efd106fef00811aa0133c1bd796b1956b8
VAS-QZ <vas.qiaozhe@gmail.com>
2019-02-12 00:21:58 +0800

    1

--- Jvm/src/main/java/Main.java
+++ Jvm/src/main/java/Main.java
@@ -3,7 +3,6 @@ import sun.misc.Unsafe;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main {
 
@@ -23,8 +22,33 @@ public class Main {
 //            throw e;
 //        }
 //        main.stackOOM();
-        main.metaSpaceOOM();
+//        main.metaSpaceOOM(); //未完成
 //        main.directMemoryOOM();
+        main.testAllocation();
+    }
+
+    /**
+     * 测试分代分配
+     * 参数：
+     * -Xms20M
+     * -Xmx20M
+     * -Xmn10M
+     * -XX:+PrintGCDetails
+     * -XX:SurvivorRatio=8
+     * -XX:+UseSerialGC
+     * -XX:PretenureSizeThreshold=5242880
+     */
+    private void testAllocation() {
+        byte[] allocation1,allocation2,allocation3,allocation4,allocation5;
+
+        allocation1 = new byte[4 * 1024*1024];
+        //eden区有8M，只用了4M为什么就要进行minor gc
+        allocation3 = new byte[2 * 1024*1024];
+
+
+//        allocation3 = new byte[3*512*1024];
+//        //为什么接近极限时会产生full gc
+//        allocation4 = new byte[512*1024];
     }
 
     /**
