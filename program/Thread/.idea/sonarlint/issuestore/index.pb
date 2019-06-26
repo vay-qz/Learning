dbe0e891898455db046f001db330b433bec50c9d
qiaozhe <qiaozhe@thunisoft.com>
2019-06-26 20:22:39 +0800

    1

--- program/Thread/.idea/sonarlint/issuestore/index.pb
+++ program/Thread/.idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,7 @@ P
  src/main/java/pers/vay/Main.java,f\7\f77ef238be53da3e88c97d4498a0820e068307b9
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Y
+)src/main/java/qz/thread/ServerThread.java,1\1\11ee6669e5afc3c02f15af15cc515df00fb77fd2
+b
+2src/test/java/pers/vay/Lock/ReentrantLockTest.java,4\e\4e71c3f93ba46606632e13d6b31ec9feb3ab68dd
--- program/Thread/src/test/java/pers/vay/Lock/ReentrantLockTest.java
+++ program/Thread/src/test/java/pers/vay/Lock/ReentrantLockTest.java
@@ -12,12 +12,12 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLockTest {
 
-    private BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(10);
 
     private final static String TARGET_STRING = "1";
 
     @Test
     public void reentrantLock_same_synchronized_success_get_lock() {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(10);
         Lock lock = new ReentrantLock();
         Thread thread = new ReentrantLockThread(lock, blockingQueue);
         thread.start();
@@ -30,6 +30,7 @@ public class ReentrantLockTest {
 
     @Test
     public void reentrantLock_same_synchronized_fail_get_lock() {
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(10);
         Lock lock = new ReentrantLock();
         lock.lock();
         Thread thread = new ReentrantLockThread(lock, blockingQueue);
@@ -41,9 +42,50 @@ public class ReentrantLockTest {
         }
     }
 
-    public void reentrantLock_fairlock_getlock_inorder() {
+    @Test
+    public void reentrantLock_fairlock_getlock_inorder() throws InterruptedException {
+        BlockingQueue<String> results = new ArrayBlockingQueue<>(10);
+        //保证线程执行完毕
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(10);
         Lock lock = new ReentrantLock(true);
-        
+        lock.lock();
+        StringBuffer origin = new StringBuffer();
+        for(int i = 1; i < 5; i++) {
+            origin.append("thread" + i);
+            Thread thread = new FairReentrantLockThread("thread" + i, lock, results, blockingQueue);
+            thread.start();
+//            Thread.sleep(500);
+        }
+        lock.unlock();
+        while(blockingQueue.size()!=4);
+        StringBuffer now = new StringBuffer();
+        for(int i = 0; i < 4; i ++) {
+            now.append(results.take());
+        }
+        Assert.assertEquals(origin.toString(), now.toString());
+    }
+
+    @Test
+    public void reentrantLock_norfairlock_getlock_inorder() throws InterruptedException {
+        BlockingQueue<String> results = new ArrayBlockingQueue<>(100);
+        //保证线程执行完毕
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(100);
+        Lock lock = new ReentrantLock();
+        lock.lock();
+        StringBuffer origin = new StringBuffer();
+        for(int i = 1; i < 20; i++) {
+            origin.append("thread" + i);
+            Thread thread = new FairReentrantLockThread("thread" + i, lock, results, blockingQueue);
+            thread.start();
+            Thread.sleep(500);
+        }
+        lock.unlock();
+        while(blockingQueue.size()!=19);
+        StringBuffer now = new StringBuffer();
+        for(int i = 0; i < 19; i ++) {
+            now.append(results.take());
+        }
+        Assert.assertNotEquals(origin.toString(), now.toString());
     }
 
     @Test
@@ -56,7 +98,7 @@ public class ReentrantLockTest {
 
     }
 
-    class ReentrantLockThread extends Thread{
+    class ReentrantLockThread extends Thread {
         private Lock lock;
         private BlockingQueue<String> blockingQueue;
 
@@ -68,12 +110,43 @@ public class ReentrantLockTest {
         @Override
         public void run() {
             try {
-                if(this.lock.tryLock()) {
+                if(this.lock.tryLock(2, TimeUnit.SECONDS)) {
                     this.blockingQueue.offer(TARGET_STRING);
+                    System.out.println(this);
+                }else {
+                    System.out.println(this + " getlock fail");
                 }
+            } catch (InterruptedException e) {
+                System.out.println(this + " getlock fail");
             } finally {
                 this.lock.unlock();
             }
         }
     }
+
+    class FairReentrantLockThread extends Thread {
+        private Lock lock;
+        private BlockingQueue<String> results;
+        private BlockingQueue<String> blockingQueue;
+
+        FairReentrantLockThread(String name, Lock lock, BlockingQueue results, BlockingQueue blockingQueue) {
+            super(name);
+            this.lock = lock;
+            this.results = results;
+            this.blockingQueue = blockingQueue;
+        }
+
+        @Override
+        public void run() {
+            try {
+                lock.lock();
+                results.offer(this.getName());
+                blockingQueue.offer("1");
+            } finally {
+                lock.unlock();
+            }
+        }
+
+    }
+
 }
