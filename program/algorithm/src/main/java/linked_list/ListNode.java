d9fadff34a62160bb1ca4cdec1b5203a643afbc2
VAS-QZ <vas.qiaozhe@gmail.com>
2019-07-25 23:08:20 +0800

    1

--- program/algorithm/src/main/java/linked_list/ListNode.java
+++ program/algorithm/src/main/java/linked_list/ListNode.java
@@ -6,4 +6,38 @@ public class ListNode {
     ListNode(int i) {
         this.val = i;
     }
+
+    public static ListNode reverse(ListNode head) {
+        if(head.next == null) {
+            return head;
+        }
+
+        if(head.next.next == null) {
+            ListNode tmp = head.next;
+            head.next.next = head;
+            head.next = null;
+            return tmp;
+        }
+
+        ListNode first = head;
+        ListNode targetPre = first.next;
+        ListNode target = first.next.next;
+        while(target != null) {
+            ListNode toMove = target;
+            target = target.next;
+
+            toMove.next = first.next;
+            targetPre.next = target;
+            first.next = toMove;
+        }
+        ListNode last = first.next;
+        while(last.next != null) {
+            last = last.next;
+        }
+        last.next = first;
+        ListNode result = first.next;
+        first.next = null;
+        return result;
+    }
+
 }
