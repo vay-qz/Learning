c90e569dd512a51bd8d9efc018caa54f6d9160ed
qiaozhe <qiaozhe@thunisoft.com>
2020-04-10 17:05:28 +0800

    1

--- program/algorithm/src/main/java/pers/vay/Dp.java
+++ program/algorithm/src/main/java/pers/vay/Dp.java
@@ -607,9 +607,29 @@ public class Dp {
         return res;
     }
 
+    /**给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字）
+     * @param nums
+     * @return
+     */
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = imax;
+                imax = imin;
+                imin = temp;
+            }
+            imax = imax * nums[i] > nums[i] ? imax * nums[i] : nums[i];
+            imin = imin * nums[i] < nums[i] ? imin * nums[i] : nums[i];
+            max = imax > max ? imax : max;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
         Dp dp = new Dp();
-        dp.printNumbers(1);
+        int[] nums = {2,3,-2,4,-2};
+        System.out.println(dp.maxProduct(nums));;
     }
 
     class CQueue {
@@ -638,7 +658,5 @@ public class Dp {
             return stack2.pop();
         }
     }
-
-
 
 }
--- program/algorithm/src/main/java/pers/vay/ListNodeProblem.java
+++ program/algorithm/src/main/java/pers/vay/ListNodeProblem.java
@@ -26,33 +26,93 @@ public class ListNodeProblem {
         swap(next1, next1.next);
     }
 
+    /**给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode res = head;
+        ListNode tail = head;
+        ListNode remove = head;
+        for (int i = 0; i < n; i++) {
+            tail = tail.next;
+        }
+        if (tail == null) {
+            return res.next;
+        }
+        tail = tail.next;
+        while (tail != null) {
+            tail = tail.next;
+            remove = remove.next;
+        }
+        remove.next = remove.next.next;
+        return res;
+    }
+
+    /**将两个升序链表合并为一个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+     * @param l1
+     * @param l2
+     * @return
+     */
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null || l2 == null) {
-            return l1 == null ? l2 : l1;
+            return l1 == null ? l2 :l1;
         }
-        ListNode head ;
-        if (l1.val < l2.val) {
-            head = l1;
-            l1 = l1.next;
-        } else {
-            head = l2;
-            l2 = l2.next;
-        }
+        ListNode head = new ListNode(0);
         ListNode res = head;
-        while (l1 != null && l2 != null) {
+        while (l1 != null && l2 !=null) {
             if (l1.val < l2.val) {
-                head.next = l1;
+                head.next = new ListNode(l1.val);
+                head = head.next;
                 l1 = l1.next;
             } else {
-                head.next = l2;
+                head.next = new ListNode(l2.val);
+                head = head.next;
                 l2 = l2.next;
             }
-            head = head.next;
         }
-        if (l1 == null || l2 == null) {
-            head.next = l1 == null ? l2 : l1;
+        head.next = l1 == null ? l2 : l1;
+        return res.next;
+    }
+
+    /**
+     * 给定一个链表，旋转链表，将链表每个节点向右移动 k 个位置，其中 k 是非负数
+     * @param head
+     * @param k
+     * @return
+     */
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null) {
+            return head;
         }
-        return res;
+        //node length
+        int length = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            temp = temp.next;
+            length++;
+        }
+        //k /= length
+        k %= length;
+        if (k == 0) {
+            return head;
+        }
+        //find newHead index
+        int index = length - k;
+        temp = head;
+        while (index > 0) {
+            temp = temp.next;
+            index--;
+        }
+        ListNode newHead = temp.next;
+        temp.next = null;
+        ListNode tail = newHead;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = head;
+        return newHead;
     }
 
     public static void main(String[] args) {
@@ -66,7 +126,9 @@ public class ListNodeProblem {
         b.next.next = new ListNode(4);
 
         ListNodeProblem problem = new ListNodeProblem();
-        problem.mergeTwoLists(a, b);
+//        problem.mergeTwoLists(a, b);
+
+        problem.rotateRight(a, 3);
     }
 
 }
